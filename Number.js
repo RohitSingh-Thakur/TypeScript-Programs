e3dac788ec83f03ee8766b3f087511fb806a2d15
RohitSingh <thakurrohitdsingh@gmail.com>
2024-07-16 00:31:25 +0530

    initial commit

---
+++ Number.js
@@ -0,0 +1,10 @@
+var number1 = 10; //Decimal Number
+var number2 = 10.12; //Float Number
+var number3 = 83; //Octal Number
+var number4 = 0xface; //Hexa-Decimal Number
+var number5 = 10; //Binary Number
+console.log("Decimal Number: ", number1);
+console.log("Float Number: ", number2);
+console.log("Octal Number: ", number3);
+console.log("Hexa-Decimal Number: ", number4);
+console.log("Binary Number: ", number5);
---
+++ Number.ts
@@ -0,0 +1,11 @@
+var number1: number = 10; //Decimal Number
+var number2: number = 10.12; //Float Number
+var number3: number = 0o123; //Octal Number
+var number4: number = 0xface; //Hexa-Decimal Number
+var number5: number = 0b1010; //Binary Number
+
+console.log("Decimal Number: ", number1);
+console.log("Float Number: ", number2);
+console.log("Octal Number: ", number3);
+console.log("Hexa-Decimal Number: ", number4);
+console.log("Binary Number: ", number5);
